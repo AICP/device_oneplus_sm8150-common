9d6ef10183141226df85e5318bbeaa171e710c54
Ralf Luther <luther.ralf@gmail.com>
2020-01-09 23:42:30 +0100

    sm8150-common: merge the overlay folders
    merge all overlays into one folder and while we are at it:
    - exclude Snap overlays from RRO enforcing
    - define default brightness according to Omni brightness settings from device
    - adapt the values to display the CPU monitor values correctly
    - enable burn-in protection for the statusbar
    - include the needed padding on lockscreen for the FoD
    - enable more CellBroadcast overlays
    - enable AGPS overlay
    - enable Volumepanel showup on the left display side
    - enable Google Colour modes for SRGB
    - enable API2 for Lineage Snap camera
    - define 4 cameras for Snap
    - enable bokeh mode for Snap camera
    - add some overlays for SnapDragonCamera(2)
    - enable tty mode for Telecomm services
    - enable a few bools for Telephony like handover and hac
    
    Change-Id: I097b1bf03c430c88ed3fcbd608a935fc26cf195c

--- common.mk
+++ common.mk
@@ -19,11 +19,13 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aicp
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # A/B
 AB_OTA_UPDATER := true
