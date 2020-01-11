9e8d44b31d705c911c6a3bd5e20f229864be3f12
Ralf Luther <luther.ralf@gmail.com>
2020-01-11 18:53:41 +0100

    sm8150-common: properly rename the service
    Change-Id: If58d495ee8eadd60ee457d04b9a040f461fb64f7

--- camera_motor/service.cpp
+++ camera_motor/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.camera.motor@1.0-service.msmnile"
+#define LOG_TAG "vendor.lineage.camera.motor@1.0-service.oneplus_msmnile"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
