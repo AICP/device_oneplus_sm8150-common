c5b08bdae005d7d873f915b5bd8e758e8bfe3dba
Julian Veit <claymore1298@gmail.com>
2025-06-01 19:24:39 +0200

    sm8150-common: overlay-aicp: add CPUInfo config
    Change-Id: Ia4a7d3849ba1dd963627b16072c5297cba7ff717

--- common.mk
+++ common.mk
@@ -234,6 +234,7 @@ $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
